51e66dffe536e099f9ff9231ff6ce9bd090f3249
unknown <STemnikov@OPT990-RD-ST.private.4d.fr>
2012-11-30 16:44:10 +0100

    #7

--- gitTest20/first.js
+++ gitTest20/first.js
@@ -6,3 +6,8 @@ var		chetvertyj = "chetyre";
 // second wave
 var		second = "two";
 var		third = "three";
+
+// third wave after publishing on real remote
+var		fourth = "four"
+var		pjatyj = "pjat"
+
